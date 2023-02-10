b8363e2a1cb0c753bff4bfd33969f41e3a7e465a
Valeriia Kraft <airkraft20@gmail.com>
2023-02-10 14:51:22 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 41780 ;
+        int costOneMile = 20 ;
+        int result = amount/costOneMile ;
+
+        System.out.println(result) ;
+    }
+}
